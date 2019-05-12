a1bdc9cd751baa808ef2dcffdaa12b93fb48493d
wyskShiro <wyskanashiro@gmail.com>
2019-05-12 19:08:28 -0400

    Organizado a ordem das tags de textView do card de personagem.

--- app/src/main/java/com/example/jera_starwars/model/dataclass/Character.kt
+++ app/src/main/java/com/example/jera_starwars/model/dataclass/Character.kt
@@ -5,15 +5,11 @@ import java.io.Serializable
 class Character : Serializable, Resource {
     lateinit var name: String
     lateinit var gender: String
-
+    lateinit var eye_color: String
+    lateinit var hair_color: String
 
     lateinit var birth_year: String
-    lateinit var eye_color: String
-
-
-    lateinit var hair_color: String
     lateinit var height: String
-
     lateinit var mass: String
     lateinit var skin_color: String
 
